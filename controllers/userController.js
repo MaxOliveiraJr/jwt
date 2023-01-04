06a518c981becb5cf5925e3a67322b77d278b8c2
Max Oliveira <max-oliveira@outlook.com.br>
2023-01-03 22:05:23 -0300

    feat: encrypt password create

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,11 +1,16 @@
 const User = require('../models/User');
+const bcrypt = require('bcryptjs');
 
 userController = {
     register: async function register(req, res) {
+
+        const validateEmail = await User.findOne({ email: req.body.email });
+        if (validateEmail) return res.status(400).send('E-mail exist');
+
         const user = new User({
             name: req.body.name,
             email: req.body.email,
-            password: req.body.password
+            password: bcrypt.hashSync(req.body.password)
         })
 
         try {
--- models/User.js
+++ models/User.js
@@ -2,10 +2,10 @@ const mongoose = require('mongoose');
 
 const userSchema = mongoose.Schema({
     name: { type: String, required: true, minlength: 3, maxlength: 50 },
-    email: { type: String, required: true, minlength: 3, maxlength: 100 },
+    email: { type: String, required: true, minlength: 3, maxlength: 100, unique: true },
     password: { type: String, required: true, minlength: 6, maxlength: 200 },
     createdAt: { type: Date, default: Date.now }
 
 })
 
-module.exports = mongoose.model('User',userSchema);
+module.exports = mongoose.model('User', userSchema);
